d5ec5a7e20aad5e582b4e87c8780255820185d40
MalikFarooq <farooq@glucorx.co.uk>
2025-04-24 17:56:31 +0500

    implemented API into usecases

--- app/src/main/java/com/example/composequotesapp/domain/usecases/GetAllQuotesUseCase.kt
+++ app/src/main/java/com/example/composequotesapp/domain/usecases/GetAllQuotesUseCase.kt
@@ -2,6 +2,7 @@ package com.example.composequotesapp.domain.usecases
 
 import com.example.composequotesapp.domain.models.Quotes
 import com.example.composequotesapp.domain.repository.QuotesRepository
+import com.example.composequotesapp.utils.ApiResponse
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -12,8 +13,22 @@ import javax.inject.Inject
 class GetAllQuotesUseCase @Inject constructor(
     private val repository: QuotesRepository
 ) {
-    operator fun invoke(): Flow<List<Quotes>> = flow {
-        val quotes = repository.getAllQuotes()
-        emit(quotes)
+    operator fun invoke(): Flow<ApiResponse<List<Quotes>>> = flow {
+        emit(ApiResponse.Loading)
+
+        try {
+            val quotes = repository.getAllQuotes()
+
+            if(quotes.isSuccessful){
+                val data= quotes.body()?: emptyList()
+                 emit(ApiResponse.Success(data))
+            }else{
+                emit(ApiResponse.Error("Error: ${quotes.code()} ${quotes.message()}"))
+            }
+
+        }catch (ex:Exception){
+            val errorMsg = ex.localizedMessage ?: "Unknown error"
+            emit(ApiResponse.Error("Exception: $errorMsg"))
+        }
     }
 }
--- app/src/main/java/com/example/composequotesapp/domain/usecases/RandomApiUseCase.kt
+++ app/src/main/java/com/example/composequotesapp/domain/usecases/RandomApiUseCase.kt
@@ -2,6 +2,7 @@ package com.example.composequotesapp.domain.usecases
 
 import com.example.composequotesapp.domain.models.Quotes
 import com.example.composequotesapp.domain.repository.QuotesRepository
+import com.example.composequotesapp.utils.ApiResponse
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
@@ -14,10 +15,23 @@ import javax.inject.Inject
 
 //TODO: need to add error handle into API
 class RandomApiUseCase @Inject constructor(
-     private  val quotesRepository: QuotesRepository
+    private val quotesRepository: QuotesRepository
 ) {
-    operator fun invoke(): Flow<Quotes> = flow {
-        val quotes = quotesRepository.getRandomQuote()
-        emit(quotes)
+    operator fun invoke(): Flow<ApiResponse<Quotes>> = flow {
+        emit(ApiResponse.Loading)
+
+        try {
+            val quotes = quotesRepository.getRandomQuote()
+
+            if (quotes.isSuccessful) {
+                emit(ApiResponse.Success(quotes.body() ?: Quotes(id = -1, author = "", quote = "")))
+            } else {
+                emit(ApiResponse.Error("Error: ${quotes.code()} ${quotes.message()}"))
+            }
+
+        } catch (ex: Exception) {
+            val errorMsg = ex.localizedMessage ?: "Unknown error"
+            emit(ApiResponse.Error("Exception: $errorMsg"))
+        }
     }
 }
